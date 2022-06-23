5fd6dc5d179cc173865b716746e3522fe6de8109
Ronald A. Richardson <me@ron.dev>
2022-06-23 10:49:28 +0800

    rollup esm bundled

---
+++ rollup.config.js
@@ -0,0 +1,69 @@
+// rollup.config.js
+import { terser } from 'rollup-plugin-terser';
+// import { eslint } from 'rollup-plugin-eslint';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+import pkg from './package.json';
+
+const input = ['src/index.js'];
+
+export default [
+    {
+        // umd
+        input,
+        plugins: [
+            nodeResolve({
+                browser: true,
+                modulesOnly: true,
+            }),
+            babel({
+				babelHelpers: 'bundled',
+			}),
+            terser(),
+        ],
+        output: [
+            {
+                file: `dist/${pkg.name}.min.js`,
+                format: 'umd',
+                name: 'leaflet-routing-machine',
+                esModule: false,
+                exports: 'named',
+                sourcemap: true,
+            },
+        ],
+        watch: {
+            exclude: ['node_modules/**'],
+            include: ['lib/**'],
+        },
+        external: ['@mapbox/corslite', '@mapbox/polyline']
+    },
+    {
+        // esm and cjs
+        input,
+        plugins: [
+            nodeResolve({
+                browser: true,
+                modulesOnly: true,
+            }),
+			babel({
+				babelHelpers: 'bundled',
+			}),
+			terser()
+        ],
+        output: [
+            {
+                dir: 'dist/esm',
+                format: 'esm',
+                exports: 'named',
+                sourcemap: true,
+            },
+            {
+                dir: 'dist/cjs',
+                format: 'cjs',
+                exports: 'named',
+                sourcemap: true,
+            },
+        ],
+        external: ['@mapbox/corslite', '@mapbox/polyline']
+    },
+];
